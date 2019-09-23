1cd2698e686196d8011866ae71b0e139bda22c27
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-09-23 15:15:10 +0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state={
+      people: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp=>resp.json())
+    .then(json=>this.setState({people: json.people}))
+    // .then(json=>console.log(json))
+  }
+
+  renderPeople = () => {
+    console.log(this.state)
+    this.state.people.map(p=><p>p.name</p>)
+  }
+
+  render(){
+    return(
+      <div className='people'>
+      {this.renderPeople()}
+      </div>
+    )
+  }
+
+}
